d44c5dcf46082ad3e0298afec72fbb0a718a5d7b
first_name last_name <my_name@example.com>
2022-05-18 12:46:23 +0500

    hello

---
+++ script.js
@@ -0,0 +1,24 @@
+let weather = +prompt('age')
+
+
+if(weather  < 3) {
+    alert("winter");
+}
+else if (weather < 6) {
+    alert("spring");
+}
+else if (weather < 9) {
+    alert("summer");
+}
+else if (weather < 12) {
+    alert("autumn");
+}
+else if (weather < 13) {
+    alert("winter");
+}
+else {
+    alert("no current season")
+}
+
+
+
